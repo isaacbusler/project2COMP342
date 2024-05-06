65542fc608de5abbeba4c0b67bc271ef85c29bae
isaacbusler <isaacbusler@gmail.com>
2024-05-05 23:29:26 -0400

    tests 1 and 3 now work

--- src/HTTPClient.java
+++ src/HTTPClient.java
@@ -1,5 +1,178 @@
 
 import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class HTTPClient {
+
+    public static final int PORT = 80;
+    public static String SERVER_ADDR = "127.0.0.1" ; // "www.google.com";
+    public static final String CRLF = "\r\n";
+    public static final String EOH = CRLF + CRLF;
+    public static final int CHUNK_SIZE = 512;				// size of fragment to process
+
+
+    //GLOBAL VARIABLES ADDED FROM PROJECT 1
+    private static String currentDirectory; //String of the current path
+    private static Path currentPath; //the path of the client
+    private static int numBytes; //number of bytes in the message being sent or received
+
+    public static void main(String[] args) {
+
+        System.out.println("client is requesting ... ");
+        try {
+
+            Scanner scanner = new Scanner(System.in);
+            String fileName;
+            // Gets the request from the user
+            String message = scanner.nextLine().trim();
+            Scanner messageReader = new Scanner(message);
+            // Gets the ip address from the user
+            SERVER_ADDR = messageReader.next();
+
+            // TODO: receive the feedback
+            System.out.println("After sending the request, wait for response: ");
+
+            //get path information
+            currentPath = Paths.get("");
+            currentPath = currentPath.resolve("client_folder");
+            currentDirectory = currentPath.toString();
+
+            if (!SERVER_ADDR.equals("127.0.0.1")) {
+                try {
+                    SERVER_ADDR = "https://" + SERVER_ADDR;
+                    URL url = new URL(SERVER_ADDR);
+                    HttpURLConnection socket = (HttpURLConnection) url.openConnection();
+                    socket.setRequestMethod("GET");
+                    DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+                    String fileNameOnly = "index.html";
+
+                    numBytes = dataInputStream.readInt();
+                    if (numBytes != -1) {
+                        byte[] fileContents = new byte[numBytes];
+                        dataInputStream.read(fileContents, 0, numBytes);
+
+                        String pathToFile = currentPath.toAbsolutePath().resolve(fileNameOnly).toString();
+
+                        FileOutputStream fileOutputStream = new FileOutputStream(pathToFile);
+
+                        fileOutputStream.write(fileContents, 0, numBytes);
+
+
+                        System.out.println("Saved file: " + fileNameOnly);
+                    } else {
+                        System.out.println("Error: HTTP/1.1 404 Not Found");
+                    }
+                } catch (MalformedURLException e) {
+                    System.out.println("Error: HTTP/1.1 404 Not Found");
+                }
+            }
+
+
+            else {
+                Socket socket = new Socket(SERVER_ADDR, PORT);
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+                // generate a HTTP request a print writer, handy to handle output stream
+                PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(dataOutputStream, StandardCharsets.ISO_8859_1));
+                printWriter.print("GET / HTTP/1.1" + CRLF);
+                printWriter.print("Host: " + SERVER_ADDR + CRLF);
+                printWriter.print("Connection: close" + CRLF);
+                printWriter.print("Accept: */*" + EOH);
+                printWriter.flush();
+
+                // if there is no file name specified it is set to "index.html"
+                if (messageReader.hasNext()) {
+                    fileName = messageReader.next();
+                } else {
+                    fileName = "index.html";
+                }
+                // Sends the file name to the Server
+                printWriter.print(fileName + EOH);
+                printWriter.flush();
+
+                // Separates the file name from the file path provided (should there be one)
+                String fileNameOnly = "";
+                char[] fileNameChars = new char[fileName.length()];
+                for (int i = 0; i < fileName.length(); i++) {
+                    fileNameChars[i] = fileName.charAt(i);
+                }
+                for (char i : fileNameChars) {
+                    fileNameOnly = fileNameOnly + i;
+                    if (i == '/') {
+                        fileNameOnly = "";
+                    }
+                }
+
+                numBytes = dataInputStream.readInt();
+                if (numBytes != -1) {
+                    byte[] fileContents = new byte[numBytes];
+                    dataInputStream.read(fileContents, 0, numBytes);
+
+                    String pathToFile = currentPath.toAbsolutePath().resolve(fileNameOnly).toString();
+
+                    FileOutputStream fileOutputStream = new FileOutputStream(pathToFile);
+
+                    fileOutputStream.write(fileContents, 0, numBytes);
+
+
+                    System.out.println("Saved file: " + fileNameOnly);
+                } else {
+                    System.out.println("Error: HTTP/1.1 404 Not Found");
+                }
+
+                printWriter.close();
+            }
+
+        }catch (UnknownHostException e){
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//CODE THAT WORKS FOR RECEIVING FILES FROM THE SERVER CLASS ON A LOOP
+
+/*
+
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
@@ -37,80 +210,81 @@ public class HTTPClient {
             //printWriter.print("GET / HTTP/1.1" + CRLF);
             //printWriter.print("Host: " + SERVER_ADDR + CRLF);
             //printWriter.print("Connection: close" + CRLF);
-            //printWriter.print("Accept: */*" + EOH);
-            //printWriter.flush();
+            //printWriter.print("Accept: *//*" + EOH);          GET RID OF ONE OF THE SLASHES
+        //printWriter.flush();
 
-            // TODO: receive the feedback
+        // TODO: receive the feedback
 
-            //get path information
-            currentPath = Paths.get("");
-            currentPath = currentPath.resolve("client_folder");
-            currentDirectory = currentPath.toString();
+        //get path information
+        currentPath = Paths.get("");
+        currentPath = currentPath.resolve("client_folder");
+        currentDirectory = currentPath.toString();
 
-            Scanner scanner = new Scanner(System.in);
-            while(true) {
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
 
-                String fileName;
-                // Gets the request from the user
-                String message = scanner.nextLine().trim();
-                Scanner messageReader = new Scanner(message);
-                // Gets the ip address from the user
-                String ipAddress = messageReader.next();
-                // Sends the ip address to the Server
-                printWriter.print(ipAddress + CRLF);
+        String fileName;
+        // Gets the request from the user
+        String message = scanner.nextLine().trim();
+        Scanner messageReader = new Scanner(message);
+        // Gets the ip address from the user
+        String ipAddress = messageReader.next();
+        // Sends the ip address to the Server
+        printWriter.print(ipAddress + CRLF);
 
-                // if there is no file name specified it is set to "index.html"
-                if (messageReader.hasNext()) {
-                    fileName = messageReader.next();
-                }
-                else {
-                    fileName = "index.html";
-                }
-                // Sends the file name to the Server
-                printWriter.print(fileName + EOH);
-                printWriter.flush();
+        // if there is no file name specified it is set to "index.html"
+        if (messageReader.hasNext()) {
+        fileName = messageReader.next();
+        }
+        else {
+        fileName = "index.html";
+        }
+        // Sends the file name to the Server
+        printWriter.print(fileName + EOH);
+        printWriter.flush();
 
-                // Separates the file name from the file path provided (should there be one)
-                String fileNameOnly = "";
-                char[] fileNameChars = new char[fileName.length()];
-                for (int i = 0; i < fileName.length(); i++) {
-                    fileNameChars[i] = fileName.charAt(i);
-                }
-                for (char i : fileNameChars)    {
-                    fileNameOnly = fileNameOnly + i;
-                    if (i == '/')   {
-                        fileNameOnly = "";
-                    }
-                }
+        // Separates the file name from the file path provided (should there be one)
+        String fileNameOnly = "";
+        char[] fileNameChars = new char[fileName.length()];
+        for (int i = 0; i < fileName.length(); i++) {
+        fileNameChars[i] = fileName.charAt(i);
+        }
+        for (char i : fileNameChars)    {
+        fileNameOnly = fileNameOnly + i;
+        if (i == '/')   {
+        fileNameOnly = "";
+        }
+        }
 
-                numBytes = dataInputStream.readInt();
-                if(numBytes != -1) {
-                    byte[] fileContents = new byte[numBytes];
-                    dataInputStream.read(fileContents, 0, numBytes);
+        numBytes = dataInputStream.readInt();
+        if(numBytes != -1) {
+        byte[] fileContents = new byte[numBytes];
+        dataInputStream.read(fileContents, 0, numBytes);
 
-                    String pathToFile = currentPath.toAbsolutePath().resolve(fileNameOnly).toString();
+        String pathToFile = currentPath.toAbsolutePath().resolve(fileNameOnly).toString();
 
-                    FileOutputStream fileOutputStream = new FileOutputStream(pathToFile);
+        FileOutputStream fileOutputStream = new FileOutputStream(pathToFile);
 
-                    fileOutputStream.write(fileContents, 0, numBytes);
+        fileOutputStream.write(fileContents, 0, numBytes);
 
 
-                    System.out.println("Saved file: " + fileNameOnly);
-                }
-                else {
-                    System.out.println("Error: HTTP/1.1 404 Not Found");
-                }
+        System.out.println("Saved file: " + fileNameOnly);
+        }
+        else {
+        System.out.println("Error: HTTP/1.1 404 Not Found");
+        }
 
-                //System.out.println("After sending the request, wait for response: ");
-                //printWriter.close();
+        //System.out.println("After sending the request, wait for response: ");
+        //printWriter.close();
 
-            }
+        }
 
         }catch (UnknownHostException e){
-            e.printStackTrace();
+        e.printStackTrace();
         }catch(IOException e){
-            e.printStackTrace();
+        e.printStackTrace();
         }
-    }
+        }
 
-}
+        }
+ */
--- src/HTTPServer.java
+++ src/HTTPServer.java
@@ -15,6 +15,154 @@ public class HTTPServer {
     public static final String CRLF = "\r\n";
     public static final String EOH = CRLF + CRLF;
 
+    //ADDED FROM PROJECT 1
+    // This will hold the path to the project
+    private static Path currentPath;
+    // This will hold the path to the server_Folder
+    private static Path pathToFolder;
+    // the string version of currentPath
+    private static String currentPathString;
+    // The string version of pathToFolder
+    private static String currentFolderString;
+    // Holds the number of bytes send by the server or recived by the server
+    private static int numBytes;
+
+    public static void main(String[] args){
+
+        System.out.println("server is listening to port 80");
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+
+            while(true){
+
+                // Handles finding the correct_path
+                currentPath = Paths.get("");
+                pathToFolder = currentPath.resolve("server_folder");
+                currentPathString = currentPath.toAbsolutePath().toString();
+                currentFolderString = pathToFolder.toString();
+
+                Socket socket = serverSocket.accept();
+                System.out.println("get connection from IP: " + socket.getRemoteSocketAddress());
+
+                DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+
+                // good to handle strings from stream
+                BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(dataInputStream));
+                String line = bufferedReader.readLine();
+                while(line != null && !line.isEmpty()){
+                    System.out.println(line);
+                    line  = bufferedReader.readLine();
+                }
+
+                //gets the file name from the input stream
+                String fileName = bufferedReader.readLine();
+                //contents from the file stored
+                byte[] fileContents = get(fileName);
+
+                // If numbytes is not -1 then the file has contents
+                if (numBytes != -1) {
+
+                    // Write the contents of the file to the new file
+                    dataOutputStream.writeInt(numBytes);
+                    dataOutputStream.write(fileContents, 0, numBytes);
+
+                    // Otherwise return -1 to tell the client that the file doesn't exist
+                } else {
+                    dataOutputStream.writeInt(-1);
+                }
+
+
+                bufferedReader.close();
+                // TODO: send back a response
+                // generate a HTTP request a print writer, handy to handle output stream
+
+
+            }
+
+        }catch (UnknownHostException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //COPIED FROM PROJECT 1
+    public static byte[] get(String fileName){
+        String pathToFile;
+        // Creates the path to the file
+        pathToFile = pathToFolder.toAbsolutePath().resolve(fileName).toString();
+
+        // teys to open the file
+        try {
+
+            // Reads the data from the file
+            File file = new File(pathToFile);
+            FileInputStream fileReader = new FileInputStream(pathToFile);
+
+
+            numBytes = (int) file.length();
+
+            // Stores teh bytes into the byte array
+            byte[] fileData = fileReader.readAllBytes();
+
+            return fileData;
+
+            // If the file couldn't be opened, set numBytes to -1
+        } catch (IOException e){
+            numBytes = -1;
+            return null;
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//CODE THAT WORKS WITH A LOOP FOR SENDING FILES TO THE CLIENT
+
+/*
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class HTTPServer {
+
+    public static final int PORT = 80;
+    public static final String IP = "127.0.0.1";
+    public static final String CRLF = "\r\n";
+    public static final String EOH = CRLF + CRLF;
+
 
     //ADDED FROM PROJECT 1
     // This will hold the path to the project
@@ -123,3 +271,4 @@ public class HTTPServer {
 
 
 }
+ */
